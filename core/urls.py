5095c0f74bf4435a2adeb07f5e32132cbfe7bb52
Gustavo <gus.marquez@duocuc.cl>
2024-06-27 23:43:39 -0400

    Guardado general

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from .views import index, registrarme, nosotros, productos
+from .views import inicio, registrarme, nosotros, productos
 from .views import usuarios, bodega, ventas, boleta, ingresar, usuarios
-from .views import misdatos, miscompras, salir, carrito, ManOfSteel, BatmanVSuperman, JusticeLeague
+from .views import misdatos, miscompras, salir, carrito, ficha
 from .views import cambiar_estado_boleta, poblar, obtener_productos, eliminar_producto_en_bodega
 from .views import premio, eliminar_producto_en_carrito, agregar_producto_al_carrito
 from .views import vaciar_carrito, mipassword, cambiar_password, comprar_ahora
@@ -16,8 +16,8 @@ from .views import vaciar_carrito, mipassword, cambiar_password, comprar_ahora
 # from .views import vaciar_carrito, mipassword, cambiar_password
 
 urlpatterns = [
-    path('inicio', index, name='inicio'),
-    path('inicio', index, name='inicio'),
+    path('inicio', inicio, name='inicio'),
+    path('inicio', inicio, name='inicio'),
     path('registrarme', registrarme, name='registrarme'),
     path('nosotros', nosotros, name='nosotros'),
     path('productos/<accion>/<id>', productos, name='productos'),
@@ -38,9 +38,7 @@ urlpatterns = [
     path('eliminar_producto_en_carrito/<carrito_id>', eliminar_producto_en_carrito, name='eliminar_producto_en_carrito'),
     path('vaciar_carrito', vaciar_carrito, name='vaciar_carrito'),
     path('agregar_producto_al_carrito/<producto_id>', agregar_producto_al_carrito, name='agregar_producto_al_carrito'),
-    path('ficha/<producto_id>', ManOfSteel, name='ficha'),
-    path('ficha/<producto_id>', BatmanVSuperman, name='ficha'),
-    path('ficha/<producto_id>', JusticeLeague, name='ficha'),
+    path('ficha/<producto_id>', ficha, name='ficha'),
     path('comprar_ahora', comprar_ahora, name='comprar_ahora'),
     path('premio', premio, name='premio'),
     path('poblar', poblar, name='poblar'),
