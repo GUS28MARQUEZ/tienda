664b3d4e96b622100c88c26700569a6275224721
Gustavo <gus.marquez@duocuc.cl>
2024-06-27 21:08:14 -0400

    MODIFICACIONES

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 
-from .views import index, ManOfSteel, BatmanVSuperman, JusticeLeague, ingresar
+from .views import index, registrarme, nosotros, productos
+from .views import usuarios, bodega, ventas, boleta, ingresar, usuarios
+from .views import misdatos, miscompras, salir, carrito, ManOfSteel, BatmanVSuperman, JusticeLeague
+from .views import cambiar_estado_boleta, poblar, obtener_productos, eliminar_producto_en_bodega
+from .views import premio, eliminar_producto_en_carrito, agregar_producto_al_carrito
+from .views import vaciar_carrito, mipassword, cambiar_password, comprar_ahora
+
 
 # from .views import index, registrarme, nosotros, admin_productos
 # from .views import admin_usuarios, admin_bodega, ventas, boleta, ingresar, admin_usuarios
@@ -10,31 +16,32 @@ from .views import index, ManOfSteel, BatmanVSuperman, JusticeLeague, ingresar
 # from .views import vaciar_carrito, mipassword, cambiar_password
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('index', index, name='index'),
-    path('ManOfSteel/<producto_id', ManOfSteel, name='ManOfSteel'),
-    path('BatmanVSuperman/<producto_id>', BatmanVSuperman, name='BatmanVSuperman'),
-    path('JusticeLeague/<producto_id>', JusticeLeague, name='JusticeLeague'),
+    path('inicio', index, name='inicio'),
+    path('inicio', index, name='inicio'),
+    path('registrarme', registrarme, name='registrarme'),
+    path('nosotros', nosotros, name='nosotros'),
+    path('productos/<accion>/<id>', productos, name='productos'),
+    path('usuarios/<accion>/<id>', usuarios, name='usuarios'),
+    path('cambiar_password', cambiar_password, name='cambiar_password'),
+    path('bodega', bodega, name='bodega'),
+    path('obtener_productos', obtener_productos, name='obtener_productos'),
+    path('eliminar_producto_en_bodega/<bodega_id>', eliminar_producto_en_bodega, name='eliminar_producto_en_bodega'),
+    path('ventas', ventas, name='ventas'),
+    path('boleta/<nro_boleta>', boleta, name='boleta'),
+    path('cambiar_estado_boleta/<nro_boleta>/<estado>', cambiar_estado_boleta, name='cambiar_estado_boleta'),
     path('ingresar', ingresar, name='ingresar'),
-    # path('registrarme', registrarme, name='registrarme'),
-    # path('nosotros', nosotros, name='nosotros'),
-    # path('admin_productos/<accion>/<id>', admin_productos, name='admin_productos'),
-    # path('admin_usuarios/<accion>/<id>', admin_usuarios, name='admin_usuarios'),
-    # path('cambiar_password', cambiar_password, name='cambiar_password'),
-    # path('admin_bodega', admin_bodega, name='admin_bodega'),
-    # path('obtener_productos', obtener_productos, name='obtener_productos'),
-    # path('eliminar_producto_en_bodega/<bodega_id>', eliminar_producto_en_bodega, name='eliminar_producto_en_bodega'),
-    # path('ventas', ventas, name='ventas'),
-    # path('boleta/<nro_boleta>', boleta, name='boleta'),
-    # path('cambiar_estado_boleta/<nro_boleta>/<estado>', cambiar_estado_boleta, name='cambiar_estado_boleta'),    
-    # path('misdatos', misdatos, name='misdatos'),
-    # path('mipassword', mipassword, name='mipassword'),
-    # path('miscompras', miscompras, name='miscompras'),
-    # path('salir', salir, name='salir'),
-    # path('carrito', carrito, name='carrito'),
-    # path('eliminar_producto_en_carrito/<carrito_id>', eliminar_producto_en_carrito, name='eliminar_producto_en_carrito'),
-    # path('vaciar_carrito', vaciar_carrito, name='vaciar_carrito'),
-    # path('agregar_producto_al_carrito/<producto_id>', agregar_producto_al_carrito, name='agregar_producto_al_carrito'),
-    # path('premio', premio, name='premio'),
-    # path('poblar', poblar, name='poblar'),
+    path('misdatos', misdatos, name='misdatos'),
+    path('mipassword', mipassword, name='mipassword'),
+    path('miscompras', miscompras, name='miscompras'),
+    path('salir', salir, name='salir'),
+    path('carrito', carrito, name='carrito'),
+    path('eliminar_producto_en_carrito/<carrito_id>', eliminar_producto_en_carrito, name='eliminar_producto_en_carrito'),
+    path('vaciar_carrito', vaciar_carrito, name='vaciar_carrito'),
+    path('agregar_producto_al_carrito/<producto_id>', agregar_producto_al_carrito, name='agregar_producto_al_carrito'),
+    path('ficha/<producto_id>', ManOfSteel, name='ficha'),
+    path('ficha/<producto_id>', BatmanVSuperman, name='ficha'),
+    path('ficha/<producto_id>', JusticeLeague, name='ficha'),
+    path('comprar_ahora', comprar_ahora, name='comprar_ahora'),
+    path('premio', premio, name='premio'),
+    path('poblar', poblar, name='poblar'),
 ]
