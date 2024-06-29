cf08fc9bdda2045eb68f44f6f14dbff85cb7e626
Gustavo <gus.marquez@duocuc.cl>
2024-06-29 19:27:24 -0400

    forms

--- core/forms.py
+++ core/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.forms import ModelForm, Form 
 from django.contrib.auth.models import User 
 from django.contrib.auth.forms import UserCreationForm 
-from .models import Categoria, Producto, Bodega, Perfil
+from .models import Categoria, Producto, bodega, Perfil
 
 class ProductoForm(ModelForm):
     class Meta:
@@ -18,9 +18,36 @@ class ProductoForm(ModelForm):
                 'descuento_oferta': 'Oferta(%)',
         }
 
+class BodegaForm(Form):
+    categoria = forms.ModelChoiceField(queryset=Categoria.objects.all(), label='Categoría')
+    producto = forms.ModelChoiceField(queryset=Producto.objects.none(), label='Producto')
+    cantidad = forms.IntegerField(label='Cantidad')
+    class Meta:
+        fields = '__all__'
+
 class IngresarForm(Form):
     username = forms.CharField(widget=forms.TextInput(), label='Cuenta')
     password = forms.CharField(widget=forms.PasswordInput(), label='Contraseña')
     class Meta:
         fields = ['username','password'] 
-            
+
+class RegistroUsuarioForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+
+class RegistroPerfilForm(ModelForm):
+    class Meta:
+        model = Perfil
+        fields = '__all__'
+
+class UsuarioForm(ModelForm):
+   class Meta:
+        model = User
+        fields = '__all__'
+
+class PerfilForm(ModelForm):
+    class Meta:
+        model = Perfil
+        fields = '__all__'         
