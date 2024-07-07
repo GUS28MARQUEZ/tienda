4347e57c39f759f6ce7e64403a638df2f52f375e
Gustavo <gus.marquez@duocuc.cl>
2024-07-07 16:50:43 -0400

    fichas

---
+++ core/static/core/js/ficha.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+// **************************************************************
+// Los botones de pago serán desarrollados en una versión futura
+// **************************************************************
+ $('#comprar-ahora').click(function() {
+   $('#accion').val('comprar-ahora');
+   $('#formulario-ficha').submit();
+ });
+ $('#agregar-al-carrito').click(function() {
+   $('#accion').val('agregar-al-carrito');
+   $('#formulario-ficha').submit();
+ });
+
+});
